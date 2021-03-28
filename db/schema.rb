e0f5e4b32048ae24399f5bd4664bf96ee94a55bd
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2021-03-28 14:02:22 +0200

    schema file generated by migrating db

---
+++ db/schema.rb
@@ -0,0 +1,28 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_03_28_120100) do
+
+  create_table "transactions_histories", force: :cascade do |t|
+    t.date "transaction_date"
+    t.string "bank_account"
+    t.string "contra_account"
+    t.string "contra_account_owner"
+    t.decimal "amount"
+    t.string "currency"
+    t.string "funding_type"
+    t.string "desc"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
