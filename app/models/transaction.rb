95e64fe65f167bab2f7ee8f0fb1ec0f215250a61
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2021-03-28 16:02:31 +0200

    basic validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,13 @@
 class Transaction < ApplicationRecord
-validates :currency,  presence: true
+# All entries except desc should be filled in:
+    validates :currency,  :bank_account,   :contra_account, :contra_account_owner,   :amount,  :funding_type,  presence: true
+# Only numbers:
+validates :amount, numericality: true
+
+# Specific lenght
+validates :bank_account, length: { is: 22 }
+validates :contra_account, length: { is: 22 }
+
+
 
 end
